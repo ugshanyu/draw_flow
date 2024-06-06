b99ec52b91ab29c9ab1d7a821e28366def8f8c1b
Uuganbayar <ugshanyu@gmail.com>
2024-06-06 08:59:43 +0800

    hello

--- src/components/drawflow.vue
+++ src/components/drawflow.vue
@@ -47,6 +47,7 @@ import Node3 from './nodes/node3.vue'
 import BasePrompt from './nodes/base_prompt.vue'
 import CasePrompt from './nodes/case_prompt.vue'
 import Input from './nodes/input.vue'
+import Add from './nodes/add.vue'
 
 
 
@@ -94,6 +95,13 @@ export default {
           color: 'darkblue',
           item: 'input',
           input:0,
+          output:1
+        },
+        {
+          name: 'Add',
+          color: 'darkpurple',
+          item: 'add',
+          input:2,
           output:1
         }
     ])
@@ -171,8 +179,91 @@ export default {
         editor.value.registerNode('base_prompt', BasePrompt, {}, {});
         editor.value.registerNode('case_prompt', CasePrompt, {}, {});
         editor.value.registerNode('input', Input, {}, {});
+        editor.value.registerNode('add', Add, {}, {})
 
-       editor.value.import({"drawflow":{"Home":{"data":{"5":{"id":5,"name":"Node2","data":{"script":"(req,res) => {\n console.log(req);\n}"},"class":"Node2","html":"Node2","typenode":"vue","inputs":{"input_1":{"connections":[{"node":"6","input":"output_1"}]}},"outputs":{"output_1":{"connections":[]},"output_2":{"connections":[]}},"pos_x":1000,"pos_y":117},"6":{"id":6,"name":"Node1","data":{"url":"localhost/add", "method": "post"},"class":"Node1","html":"Node1","typenode":"vue","inputs":{},"outputs":{"output_1":{"connections":[{"node":"5","output":"input_1"}]}},"pos_x":137,"pos_y":89}}}}})
+       editor.value.import({
+          "drawflow": {
+            "Home": {
+              "data": {
+                "7": {
+                  "id": 7,
+                  "name": "add",
+                  "data": {},
+                  "class": "add",
+                  "html": "add",
+                  "typenode": "vue",
+                  "inputs": {
+                    "input_1": {
+                      "connections": [
+                        {
+                          "node": "8",
+                          "input": "output_1"
+                        }
+                      ]
+                    },
+                    "input_2": {
+                      "connections": [
+                        {
+                          "node": "9",
+                          "input": "output_1"
+                        }
+                      ]
+                    }
+                  },
+                  "outputs": {
+                    "output_1": {
+                      "connections": []
+                    }
+                  },
+                  "pos_x": 563,
+                  "pos_y": 198
+                },
+                "8": {
+                  "id": 8,
+                  "name": "input",
+                  "data": {},
+                  "class": "input",
+                  "html": "input",
+                  "typenode": "vue",
+                  "inputs": {},
+                  "outputs": {
+                    "output_1": {
+                      "connections": [
+                        {
+                          "node": "7",
+                          "output": "input_1"
+                        }
+                      ]
+                    }
+                  },
+                  "pos_x": 174,
+                  "pos_y": 72
+                },
+                "9": {
+                  "id": 9,
+                  "name": "input",
+                  "data": {},
+                  "class": "input",
+                  "html": "input",
+                  "typenode": "vue",
+                  "inputs": {},
+                  "outputs": {
+                    "output_1": {
+                      "connections": [
+                        {
+                          "node": "7",
+                          "output": "input_2"
+                        }
+                      ]
+                    }
+                  },
+                  "pos_x": 179,
+                  "pos_y": 365
+                }
+              }
+            }
+          }
+        })
   })
 
   return {
